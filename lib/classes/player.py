b5f9845384a91427d0e0ab3683663777babe756e
Sarah Dean <128323898+sarahadean@users.noreply.github.com>
2023-05-30 11:37:05 -0500

    tests pain-stakingly passed

--- lib/classes/player.py
+++ lib/classes/player.py
@@ -6,22 +6,58 @@ class Player:
         self.username = username
         self._results = []
         self._games_played = []
-        
+        Player.all.append(username)
+
+    @property
+    def username(self):
+        return self._username
+    
+    @username.setter
+    def username(self, username):
+        if isinstance(username, str) and 2 <= len(username) <= 16:
+            self._username = username
+        else:
+            raise Exception("Username must be between 2 and 16 characters")
+
+    # Adds new results to instance attribute player._results if new_result exists.
+    # Returns a list of Result instances associated with the Player instance.
+    # You will need to call this method in `Result.init()`.
+    
     def results(self, new_result=None):
         from classes.result import Result
-        pass
+        if isinstance(new_result, Result):
+            self._results.append(new_result)
+        return [result for result in Result.all if result.player == self]
     
+    #Returns a list of Game instances played by the Player instance:
+    #taking in new game, appending to _games_played list and returning list of this player(self)'s
+    #games played
     def games_played(self, new_game=None):
         from classes.game import Game
-        pass
-    
+        from classes.result import Result
+        if isinstance(new_game, Game):
+            self._games_played.append(new_game)
+        return [result.game for result in Result.all if result.player == self]
+
     def played_game(self, game):
-        pass
+        # return game in self._games_played
+        from classes.result import Result
+        return len([result.game for result in Result.all if result.player == self and result.game == game]) > 0
     
+    #Returns the number of times the Player instance has played (Result instance created) 
+    # the Game instance
     def num_times_played(self, game):
-        pass
+        from classes.result import Result
+        #go thru self._games_played and count number of times game appears
+        # return self._games_played.count(game)
+        return len([result.game for result in Result.all if result.player == self and result.game == game])
     
     @classmethod
     def highest_scored(cls, game):
         pass
         
+#-----testing-----
+
+# user_one = Player("Mamahellcat")
+# user_two = Player("a")
+# print(user_two.username)
--- lib/classes/result.py
+++ lib/classes/result.py
@@ -6,3 +6,50 @@ class Result:
         self.player = player
         self.game = game
         self.score = score
+        player.results(self)
+        player.games_played(game)
+        game.results(self)
+        game.players(player)
+        Result.all.append(self)
+
+    @property
+    def score(self):
+        return self._score
+    
+    @score.setter
+    def score(self, score):
+        if isinstance(score, int) and 1 <= score <= 5000:
+            self._score = score
+        else:
+            raise Exception("Invalid")
+        
+    # Returns the player for the Result
+    
+    @property
+    def player(self):
+        return self._player
+    
+    #player must be Player instances
+    @player.setter
+    def player(self, player):
+        from classes.player import Player
+        if isinstance(player, Player):
+            self._player = player
+        else:
+            raise Exception("Invalid")
+        
+    @property
+    def game(self):
+        return self._game
+    
+    @game.setter
+    def game(self, game):
+        from classes.game import Game
+        if isinstance(game, Game):
+            self._game = game
+        else:
+            raise Exception
+   
+
+# result = Result("Sarah", "Tetris", 0)
+# print(result.score)
